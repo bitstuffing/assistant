65dac4ff00194291ed803e235e735d2668f4c163
Bit <bitstuffing@gmail.com>
2019-09-21 17:39:26 +0200

    clean

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import signal
 
 interrupted = False
 
-#logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
-#logging.getLogger().addHandler(logging.StreamHandler())
-
 logPath = "/home/pi/assistant"
 fileName = "logfile"
 
